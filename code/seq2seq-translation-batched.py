04fc94e2e2aa913450f91cf7af5b1de6b9bc9d1d
Alex <fan@cs.ubc.ca>
2018-11-08 14:38:26 -0800

    no_grad

--- code/seq2seq-translation-batched.py
+++ code/seq2seq-translation-batched.py
@@ -840,61 +840,60 @@ def time_since(since, percent):
 
 
 def evaluate(input_seq, max_length=MAX_LENGTH):
-    #with torch.no_grad(): 
-#     input_lengths = [len(input_seq)]
-    input_seqs = [indexes_from_sentence(input_lang, input_seq)]
-    input_lengths = [len(input_seq) for input_seq in input_seqs]
-    input_batches = Variable(torch.LongTensor(input_seqs), volatile=True).transpose(0, 1)
-    
-    if USE_CUDA:
-        input_batches = input_batches.cuda()
+    with torch.no_grad(): 
+        input_seqs = [indexes_from_sentence(input_lang, input_seq)]
+        input_lengths = [len(input_seq) for input_seq in input_seqs]
+        input_batches = Variable(torch.LongTensor(input_seqs), volatile=True).transpose(0, 1)
         
-    print(input_lengths)
-        
-    # Set to not-training mode to disable dropout
-    encoder.train(False)
-    decoder.train(False)
-    
-    # Run through encoder
-    encoder_outputs, encoder_hidden = encoder(input_batches, input_lengths, None)
-
-    # Create starting vectors for decoder
-    decoder_input = Variable(torch.LongTensor([SOS_token]), volatile=True) # SOS
-    decoder_hidden = encoder_hidden[:decoder.n_layers] # Use last (forward) hidden state from encoder
-    
-    if USE_CUDA:
-        decoder_input = decoder_input.cuda()
-
-    # Store output words and attention states
-    decoded_words = []
-    decoder_attentions = torch.zeros(max_length + 1, max_length + 1)
-    
-    # Run through decoder
-    for di in range(max_length):
-        decoder_output, decoder_hidden, decoder_attention = decoder(
-            decoder_input, decoder_hidden, encoder_outputs
-        )
-        decoder_attentions[di,:decoder_attention.size(2)] += decoder_attention.squeeze(0).squeeze(0).cpu().data
-
-        # Choose top word from output
-        topv, topi = decoder_output.data.topk(1)
-        ni = topi[0][0]
-        if ni == EOS_token:
-            decoded_words.append('<EOS>')
-            break
-        else:
-#             print(ni)
-            decoded_words.append(output_lang.index2word[int(ni)])
+        if USE_CUDA:
+            input_batches = input_batches.cuda()
             
-        # Next input is chosen word
-        decoder_input = Variable(torch.LongTensor([ni]))
-        if USE_CUDA: decoder_input = decoder_input.cuda()
+        print(input_lengths)
+            
+        # Set to not-training mode to disable dropout
+        encoder.train(False)
+        decoder.train(False)
+        
+        # Run through encoder
+        encoder_outputs, encoder_hidden = encoder(input_batches, input_lengths, None)
 
-    # Set back to training mode
-    encoder.train(True)
-    decoder.train(True)
-    
-    return decoded_words, decoder_attentions[:di+1, :len(encoder_outputs)]
+        # Create starting vectors for decoder
+        decoder_input = Variable(torch.LongTensor([SOS_token]), volatile=True) # SOS
+        decoder_hidden = encoder_hidden[:decoder.n_layers] # Use last (forward) hidden state from encoder
+        
+        if USE_CUDA:
+            decoder_input = decoder_input.cuda()
+
+        # Store output words and attention states
+        decoded_words = []
+        decoder_attentions = torch.zeros(max_length + 1, max_length + 1)
+        
+        # Run through decoder
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            decoder_attentions[di,:decoder_attention.size(2)] += decoder_attention.squeeze(0).squeeze(0).cpu().data
+
+            # Choose top word from output
+            topv, topi = decoder_output.data.topk(1)
+            ni = topi[0][0]
+            if ni == EOS_token:
+                decoded_words.append('<EOS>')
+                break
+            else:
+    #             print(ni)
+                decoded_words.append(output_lang.index2word[int(ni)])
+                
+            # Next input is chosen word
+            decoder_input = Variable(torch.LongTensor([ni]))
+            if USE_CUDA: decoder_input = decoder_input.cuda()
+
+        # Set back to training mode
+        encoder.train(True)
+        decoder.train(True)
+        
+        return decoded_words, decoder_attentions[:di+1, :len(encoder_outputs)]
 
 
 # We can evaluate random sentences from the training set and print out the input, target, and output to make some subjective quality judgements:

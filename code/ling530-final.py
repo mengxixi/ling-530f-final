2891a3a98b2b39e8068f0af47440763af66af9ab
Si Yi Meng <mengxixi@hotmail.com>
2018-11-15 23:27:36 -0800

    vectorize attention to speed up

--- code/ling530-final.py
+++ code/ling530-final.py
@@ -372,33 +372,9 @@ class Attn(nn.Module):
             self.v = nn.Parameter(torch.FloatTensor(1, hidden_size))
 
     def forward(self, hidden, encoder_outputs):
-        max_len = encoder_outputs.size(0)
-        this_batch_size = encoder_outputs.size(1)
-
-        # Create variable to store attention energies
-        attn_energies = Variable(torch.zeros(this_batch_size, max_len)) # B x S
-
-        attn_energies = attn_energies.to(device)
-
-        # For each batch of encoder outputs
-        for b in range(this_batch_size):
-            # Calculate energy for each encoder output
-            for i in range(max_len):
-                attn_energies[b, i] = self.score(hidden[:, b], encoder_outputs[i, b].unsqueeze(0))
-
+        attn_energies = torch.bmm(hidden.transpose(0,1), encoder_outputs.permute(1,2,0)).squeeze(1)
         # Normalize energies to weights in range 0 to 1, resize to 1 x B x S
         return F.softmax(attn_energies, dim=1).unsqueeze(1)
-    
-    def score(self, hidden, encoder_output):
-        if self.method == 'dot':
-            energy =torch.dot(hidden.view(-1), encoder_output.view(-1))
-        elif self.method == 'general':
-            energy = self.attn(encoder_output)
-            energy = torch.dot(hidden.view(-1), energy.view(-1))
-        elif self.method == 'concat':
-            energy = self.attn(torch.cat((hidden, encoder_output), 1))
-            energy = torch.dot(self.v.view(-1), energy.view(-1))
-        return energy
 
 
 class DecoderRNN(nn.Module):

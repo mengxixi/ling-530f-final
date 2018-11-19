946690c99ab1c7fd3956b954d773297254c28602
Si Yi Meng <mengxixi@hotmail.com>
2018-11-18 23:32:45 -0800

    experiment with elmo

--- code/ling530-final.py
+++ code/ling530-final.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 import time
 import math
@@ -14,6 +15,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
+from allennlp.modules.elmo import Elmo, batch_to_ids
 
 
 # logging configurations
@@ -62,7 +64,7 @@ MIN_TEXT_LENGTH = 5
 MIN_FREQUENCY   = 4 
 MIN_KNOWN_COUNT = 3
 
-EMBEDDING_DIM = 300
+EMBEDDING_DIM = 1024
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -143,8 +145,8 @@ else:
 
     vocab_freq_dict = {}
 
-    WORD_2_INDEX = {"PAD": 0, "SOS": 1, "EOS": 2}#, "unk": 3}
-    INDEX_2_WORD = {0: "PAD", 1: "SOS", 2: "EOS"}#, 3:"unk"}
+    WORD_2_INDEX = {"PAD": 0, "<S>": 1, "</S>": 2}#, "unk": 3}
+    INDEX_2_WORD = {0: "PAD", 1: "<S>", 2: "</S>"}#, 3:"unk"}
 
     def remove_low_freq_words(freq_dict, tokens):
         filtered_tokens = []
@@ -232,25 +234,25 @@ VOCAB_SIZE = len(WORD_2_INDEX)
 
 
 
-class GloVe():
-    def __init__(self, path, dim):
-        self.dim = dim
-        self.word_embedding_dict = {}
-        with open(path) as f:
-            for line in f:
-                values = line.split()
-                embedding = values[-dim:]
-                word = ''.join(values[:-dim])
-                self.word_embedding_dict[word] = np.asarray(embedding, dtype=np.float32)
+# class GloVe():
+#     def __init__(self, path, dim):
+#         self.dim = dim
+#         self.word_embedding_dict = {}
+#         with open(path) as f:
+#             for line in f:
+#                 values = line.split()
+#                 embedding = values[-dim:]
+#                 word = ''.join(values[:-dim])
+#                 self.word_embedding_dict[word] = np.asarray(embedding, dtype=np.float32)
     
-    def get_word_vector(self, word):
-        if word not in self.word_embedding_dict.keys():
-            embedding = np.random.uniform(low=-1, high=1, size=self.dim).astype(np.float32)
-            self.word_embedding_dict[word] = embedding
-            return embedding
-        else:
-            return self.word_embedding_dict[word]
-glvmodel = GloVe(os.path.join('..', 'models', 'glove', 'glove.6B.300d.txt'), dim=300)
+#     def get_word_vector(self, word):
+#         if word not in self.word_embedding_dict.keys():
+#             embedding = np.random.uniform(low=-1, high=1, size=self.dim).astype(np.float32)
+#             self.word_embedding_dict[word] = embedding
+#             return embedding
+#         else:
+#             return self.word_embedding_dict[word]
+# glvmodel = GloVe(os.path.join('..', 'models', 'glove', 'glove.6B.300d.txt'), dim=300)
 
 
 # ## Gather word embeddings for tokens in the training data
@@ -261,8 +263,8 @@ glvmodel = GloVe(os.path.join('..', 'models', 'glove', 'glove.6B.300d.txt'), dim
 
 
 pretrained_embeddings = []
-for i in range(VOCAB_SIZE):
-    pretrained_embeddings.append(glvmodel.get_word_vector(INDEX_2_WORD[i]))
+# for i in range(VOCAB_SIZE):
+#     pretrained_embeddings.append(glvmodel.get_word_vector(INDEX_2_WORD[i]))
 
 
 # In[ ]:
@@ -370,19 +372,22 @@ class EncoderRNN(nn.Module):
         self.dropout = dropout
         self.embed_size = embed_size
         
-        glove_embeddings = torch.tensor(pretrained_embeddings)
-        self.embedding = nn.Embedding(input_size, embed_size).from_pretrained(glove_embeddings, freeze=True)
+        # glove_embeddings = torch.tensor(pretrained_embeddings)
+        # self.embedding = nn.Embedding(input_size, embed_size).from_pretrained(glove_embeddings, freeze=True)
         
         self.gru = nn.GRU(embed_size, hidden_size, n_layers, dropout=self.dropout, bidirectional=True)
         param_init(self.gru.named_parameters())
         
     def forward(self, input_seqs, input_lengths, hidden=None):
-        embedded = self.embedding(input_seqs)
-        try:
-            packed = torch.nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
-        except:
-            print(input_seqs)
-            print(input_lengths)
+        embedded = input_seqs #self.embedding(input_seqs)
+
+        # try:
+
+        # except Exception as e:
+        #     print(e)
+        #     print(input_seqs)
+        #     print(input_lengths)
+        packed = torch.nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
         outputs, hidden = self.gru(packed, hidden)
         
         # unpack (back to padded)
@@ -421,13 +426,13 @@ class DecoderRNN(nn.Module):
 
         # Define layers
 
-        glove_embeddings = torch.tensor(pretrained_embeddings)
-        self.embedding = nn.Embedding(output_size, hidden_size).                from_pretrained(glove_embeddings, freeze=True)
+        # glove_embeddings = torch.tensor(pretrained_embeddings)
+        # self.embedding = nn.Embedding(output_size, hidden_size).                from_pretrained(glove_embeddings, freeze=True)
 
         self.embedding_dropout = nn.Dropout(dropout)
         self.gru = nn.GRU(embed_size, hidden_size, n_layers, dropout=dropout)
         self.concat = nn.Linear(hidden_size * 2, hidden_size)
-        self.out = nn.Linear(hidden_size, 1024)
+        self.out = nn.Linear(hidden_size, 2048)
         
         # Choose attention model
         self.attn = Attn(hidden_size)
@@ -440,7 +445,7 @@ class DecoderRNN(nn.Module):
 
         # Get the embedding of the current input word (last output word)
         batch_size = input_seq.size(0)
-        embedded = self.embedding(input_seq)
+        embedded = input_seq #self.embedding(input_seq)
         embedded = self.embedding_dropout(embedded)
         embedded = embedded.view(1, batch_size, self.embed_size) # S=1 x B x N
 
@@ -509,7 +514,7 @@ def evaluate(input_seq, encoder, decoder, max_length=MAX_LENGTH):
             # topv, topi = decoder_output.data.topk(1)
             # ni = topi[0][0]
             if ni == EOS_token:
-                decoded_words.append('<EOS>')
+                decoded_words.append('</S>')
                 break
             else:
                 decoded_words.append(INDEX_2_WORD[int(ni)])
@@ -567,7 +572,7 @@ def load_checkpoint(encoder, decoder, encoder_optimizer, decoder_optimizer,  nam
 
 
 
-def train_batch(input_batches, input_lengths, target_batches, target_lengths, batch_size, encoder, decoder, encoder_optimizer, decoder_optimizer, clip):
+def train_batch(input_batches, input_lengths, input_embeds, target_batches, target_lengths, target_embeds, batch_size, encoder, decoder, encoder_optimizer, decoder_optimizer, clip):
     
     # Zero gradients of both optimizers
     encoder_optimizer.zero_grad()
@@ -577,17 +582,17 @@ def train_batch(input_batches, input_lengths, target_batches, target_lengths, ba
     input_batches = input_batches.to(device)
 
     # Run words through encoder
-    encoder_outputs, encoder_hidden = encoder(input_batches, input_lengths, None)
+    encoder_outputs, encoder_hidden = encoder(input_embeds, input_lengths, None)
     
     # Prepare input and output variables
-    decoder_input = Variable(torch.LongTensor([SOS_token] * batch_size)).to(device)
+    decoder_input = Variable(SOS_emb*batch_size).to(device)
     decoder_hidden = torch.cat((encoder_hidden[0], encoder_hidden[1]),1)
     for i in range(1, encoder.n_layers):
         decoder_hidden = torch.stack((decoder_hidden,torch.cat((encoder_hidden[i*2],encoder_hidden[i*2+1]),1)))
     decoder_hidden = decoder_hidden.to(device)
 
     max_target_length = max(target_lengths)
-    all_decoder_outputs = Variable(torch.zeros(max_target_length, batch_size, 1024)).to(device)
+    all_decoder_outputs = Variable(torch.zeros(max_target_length, batch_size, 2048)).to(device)
 
 
     # Run through decoder one time step at a time
@@ -597,7 +602,7 @@ def train_batch(input_batches, input_lengths, target_batches, target_lengths, ba
         )
 
         all_decoder_outputs[t] = decoder_output
-        decoder_input = target_batches[t] # Next input is current target
+        decoder_input = target_embeds[t] # Next input is current target
 
     # Loss calculation and backpropagation
     loss = masked_adasoft(all_decoder_outputs, target_batches, target_lengths)
@@ -630,10 +635,11 @@ def train(pairs, encoder, decoder, encoder_optimizer, decoder_optimizer, n_epoch
         
         # Get training data for this cycle
         for batch_ind, batch_data in enumerate(random_batch(batch_size, pairs)):
-            input_seqs, input_lengths, target_seqs, target_lengths = batch_data
+            input_seqs, input_lengths, target_seqs, target_lengths, input_embeds, target_embeds = batch_data
+
             # Run the train function
             loss, ec, dc = train_batch(
-                input_seqs, input_lengths, target_seqs, target_lengths, batch_size,
+                input_seqs, input_lengths, input_embeds, target_seqs, target_lengths, target_embeds, batch_size,
                 encoder, decoder,
                 encoder_optimizer, decoder_optimizer, clip
             )
@@ -664,11 +670,21 @@ def random_batch(batch_size, data):
     # Choose random pairs
     for i in range(0, end_index, batch_size):
         pairs = data[i:i+batch_size]
+
+        input_sents = [pair[1] for pair in pairs]
+        char_ids = batch_to_ids(input_sents)
+        input_embeds = elmo(char_ids)["elmo_representations"]
+
+        target_sents = [pair[0] for pair in pairs]
+        char_ids = batch_to_ids(target_sents)
+        target_embeds = elmo(char_ids)["elmo_representations"]
+
         input_seqs = [indexes_from_sentence( pair[1], isHeadline=False) for pair in pairs]
         target_seqs = [indexes_from_sentence(pair[0], isHeadline=True) for pair in pairs]
-        seq_pairs = sorted(zip(input_seqs, target_seqs), key=lambda p: len(p[0]), reverse=True)
-        input_seqs, target_seqs = zip(*seq_pairs)
-    
+
+        seq_pairs = sorted(zip(input_seqs, target_seqs, input_embeds, target_embeds), key=lambda p: len(p[0]), reverse=True)
+        input_seqs, target_seqs, input_embeds, target_embeds = zip(*seq_pairs)
+
         input_lengths = [len(s) for s in input_seqs]
         input_padded = [pad_seq(s, max(input_lengths)) for s in input_seqs]
         
@@ -680,7 +696,11 @@ def random_batch(batch_size, data):
         
         input_var = input_var.to(device)
         target_var = target_var.to(device)
-        yield input_var, input_lengths, target_var, target_lengths
+
+        input_embeds = torch.stack(list(input_embeds)).squeeze(0).transpose(0,1).to(device)
+        target_embeds = torch.stack(list(target_embeds)).squeeze(0).transpose(0,1).to(device)
+
+        yield input_var, input_lengths, target_var, target_lengths, input_embeds, target_embeds
 '''
 def test(dev_text, encoder,decoder):
     logging.info("Start testing")
@@ -700,7 +720,6 @@ n_layers = 2
 dropout = 0.5
 
 batch_size = 32
-embed_size = 300
 
 # Configure training/optimization
 clip = 50.0
@@ -709,9 +728,16 @@ decoder_learning_ratio = 5.0
 n_epochs = 1
 weight_decay = 1e-4
 
+options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
+
+elmo = Elmo(options_file, weight_file, 1, dropout=0)
+
+SOS_emb = elmo(batch_to_ids([['<S>']]))["elmo_representations"][0].view(EMBEDDING_DIM)
+
 # Initialize models
-encoder = EncoderRNN(VOCAB_SIZE, hidden_size, embed_size, pretrained_embeddings, n_layers, dropout=dropout).to(device)
-decoder = DecoderRNN(2*hidden_size, VOCAB_SIZE, embed_size, pretrained_embeddings, n_layers, dropout=dropout).to(device)
+encoder = EncoderRNN(VOCAB_SIZE, hidden_size, EMBEDDING_DIM, pretrained_embeddings, n_layers, dropout=dropout).to(device)
+decoder = DecoderRNN(2*hidden_size, VOCAB_SIZE, EMBEDDING_DIM, pretrained_embeddings, n_layers, dropout=dropout).to(device)
 
 
 encoder_optimizer = torch.optim.Adam(encoder.parameters(), lr=learning_rate, weight_decay=weight_decay)
@@ -719,7 +745,7 @@ decoder_optimizer = torch.optim.Adam(decoder.parameters(), lr=learning_rate * de
 
 load_checkpoint(encoder, decoder, encoder_optimizer, decoder_optimizer, CHECKPOINT_FNAME)
 
-crit = nn.AdaptiveLogSoftmaxWithLoss(1024, VOCAB_SIZE, [1000, 20000]).to(device)
+crit = nn.AdaptiveLogSoftmaxWithLoss(2048, VOCAB_SIZE, [1000, 20000]).to(device)
 
 train(train_data, encoder, decoder, encoder_optimizer, decoder_optimizer,  n_epochs, batch_size, clip)
 
